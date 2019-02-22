cec445b7c311ffde1e09b03907f078e9545aafd4
hvnuoc <hvndoatmenh@gmail.com>
2019-02-22 13:32:54 +0700

    hvn

---
+++ TeduShop.Web/Scripts/spa/app.js
@@ -0,0 +1,10 @@
+﻿/// <reference path="../../bower_components/angular/angular.js" />
+var myApp = angular.module('myModule', []);
+
+myApp.controller("mycontroller", mycontroller);
+
+mycontroller.$inject = ['$scope'];
+//declare
+function mycontroller($scope) {
+    $scope.message = "this is my message from controller";
+}
